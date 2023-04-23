25bbc5f4b5eda2975d799f90ff19846d051b978f
zhairuijie <zhairuijie@bytedance.com>
2023-04-23 17:41:29 +0800

    https

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -39,7 +39,7 @@ class Index extends BaseController
 
     public function ping(): Json
         {
-            return self::getResponse(0, 'success', $res);
+            return self::getResponse(0, 'success');
         }
 
     public function setName(): Json
